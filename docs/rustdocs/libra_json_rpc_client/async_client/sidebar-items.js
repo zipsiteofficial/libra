24b84857de55c9a70ed502fad42439a5daf43eb1
Libra Website Deployment Script <libra-doc-bot@users.noreply.github.com>
2020-11-05 18:14:28 +0000

    [skip ci] Deploy website
    Deploy website version based on c18919e6aa6fbb705b84724b6f31c30ab70b3ad3

--- docs/rustdocs/libra_json_rpc_client/async_client/sidebar-items.js
+++ docs/rustdocs/libra_json_rpc_client/async_client/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Error",""],["UnexpectedError",""],["WaitForTransactionError",""]],"mod":[["defaults",""],["types",""]],"struct":[["Client",""],["JsonRpcError",""],["JsonRpcResponse",""],["Response",""],["Retry",""],["State",""]],"trait":[["RetryStrategy",""]]});
+initSidebarItems({"enum":[["Error",""],["UnexpectedError",""],["WaitForTransactionError",""]],"mod":[["defaults",""],["types",""]],"struct":[["Client",""],["JsonRpcError",""],["JsonRpcResponse",""],["Request",""],["Response",""],["Retry",""],["State",""]],"trait":[["RetryStrategy",""]]});
